38cbdab8f7c56e23979334dd57c1808679ce2e5b
Haeld55 <158680574+Haeld55@users.noreply.github.com>
2024-02-03 19:29:27 +0800

    Update server.js

--- server.js
+++ server.js
@@ -48,10 +48,10 @@ app.use('/api/gcash', gcashRouter)
 
 
 
-app.use(express.static(path.join(__dirname, '/client/dist')));
+app.use(express.static(path.join(__dirname, '/client/public')));
 
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'client', 'dist', 'index.html'));
+  res.sendFile(path.join(__dirname, 'client', 'public', 'index.html'));
 })
 
 app.use((err, req, res, next) => {
